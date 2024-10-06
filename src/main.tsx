487ba604cfe8686ac7231b0425e2ae94d0cec8dc
Soapman-PC <jelinsksk@gmail.com>
2024-10-06 23:22:59 +0300

    Set up router and tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import AuthPage from "./screens/AuthPage.tsx";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: '/login',
+        element: <AuthPage />
+    }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/screens/AuthPage.tsx
@@ -0,0 +1,11 @@
+
+
+const AuthPage = () => {
+    return (
+        <>
+            <h1 className="text-4xl text-red-500">Mother fucking auth page biotch</h1>
+        </>
+    )
+}
+
+export default AuthPage
