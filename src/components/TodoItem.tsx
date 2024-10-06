ad5ed3e7220cc228bdf9561ba3acc98436f4abef
Soapman-PC <jelinsksk@gmail.com>
2024-10-07 00:50:18 +0300

    Created the section for when there's no todos and started the one which displays todos. Wrapping it up for tonight, because I'm going to sleep :)

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,9 @@
+const TodoItem = (props: any) => {
+    return (
+        <div>
+            <h1>{props.todoContent}</h1>
+        </div>
+    )
+}
+
+export default TodoItem
--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,15 +1,62 @@
 import AddIcon from '../assets/plus.png'
+import clipboard from '../assets/Clipboard.png'
+
+import TodoItem from "../components/TodoItem.tsx";
 
 const HomePage = () => {
+    let todos = [
+        'feed the cats',
+        'kill the cats',
+        'pet Aino on the head',
+        'give Aino a kiss on the left cheek'
+    ];
+
     return (
         <div className="max-w-screen-lg flex flex-col justify-center mx-auto relative">
             <div className="flex justify-center space-x-2 absolute top-[-15px] w-full">
-                <input type="text" className="w-5/6 rounded-md px-5 py-3 outline-none bg-[var(--gray-500)] text-[var(--gray-200)]" placeholder="Enter Todo" />
+                <input type="text" className="w-full rounded-md px-5 py-3 outline-none bg-[var(--gray-500)] text-[var(--gray-200)]" placeholder="Enter Todo" />
                 <button onClick={() => console.log('Submit')} type="submit" className="bg-[var(--blue)] text-white px-5 rounded-md py-3 flex justify-center items-center">Add
                     <img src={AddIcon} className="ml-2" alt=""/></button>
             </div>
 
-            {/* If there are no todos */}
+
+            <div className="flex flex-col w-full">
+                <div className="flex mt-14 justify-between">
+                    <div className="flex flex-row w-fit">
+                        <h1 className="text-[var(--blue)] font-bold">Tasks Created</h1>
+                        <span className="bg-[var(--gray-400)] text-[var(--gray-200)] py-auto px-3 rounded-2xl ml-2">0</span>
+                    </div>
+
+                    <div className="flex flex-row w-fit">
+                    <h1 className="text-[var(--purple)] font-bold">Finished Tasks</h1>
+                        <span className="bg-[var(--gray-400)] text-[var(--gray-200)] py-auto px-3 rounded-2xl ml-2">0</span>
+                    </div>
+                </div>
+
+
+
+                {/* If there are no todos */}
+                { (todos.length == 0) ?
+                <div
+                    className="border-t border-t-[#333333] rounded-xl w-full flex flex-col justify-center items-center mt-10 text-center pt-20">
+                    <img className="w-fit mt-30" src={clipboard} alt=""/>
+                    <h1 className="text-[var(--gray-300)] mt-5">
+                        You don't have any tasks registered yet<br/>
+                        <span className="font-bold">Create tasks and organize your to-do items</span>
+                    </h1>
+                </div>
+
+                :
+
+                <div className="w-full flex flex-col justify-center items-center text-center mt-10">
+                    {
+                        todos.map((item, key) => {
+                            return <TodoItem todoContent={item} key={key}/>
+                        })
+                    }
+                </div>
+                }
+            </div>
         </div>
     )
 }
