5e41a320860f2d976ee6c3335dd7512eb396658c
Soapman-PC <jelinsksk@gmail.com>
2024-10-07 12:41:25 +0300

    Finished ui, I think...

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,23 @@ import AuthPage from "./screens/AuthPage.tsx";
 function App() {
     const [loggedIn, setLoggedIn] = useState(false);
 
+    const enterGuest = () => {
+        setLoggedIn(true);
+    }
 
-  return (
-    <div className="max-h-screen">
-        {
-            loggedIn ? <HomePage /> : <AuthPage />
-        }
-    </div>
-  )
+    return (
+        <div className="max-h-screen mx-2 text-white">
+            {
+                loggedIn ? <HomePage /> :
+                    <section className="flex justify-center items-center" style={{height: 'calc(100vh - 208px)' }}>
+                        <AuthPage
+                            guestEntry={enterGuest}
+                        />
+                    </section>
+
+            }
+        </div>
+    )
 }
 
 export default App
--- src/screens/AuthPage.tsx
+++ src/screens/AuthPage.tsx
@@ -1,10 +1,27 @@
+import GoogleLogo from '../assets/Google-logo.png';
 
-
-const AuthPage = () => {
+const AuthPage = (props: any) => {
     return (
-        <>
-            <h1 className="text-4xl text-red-500">Mother fucking auth page biotch</h1>
-        </>
+        <div className="flex flex-col items-center justify-center max-w-sm space-y-3 mb-32 md:m-auto">
+            <h1 className="text-4xl">Join the Club 🥪</h1>
+            <input type="text"
+                   className="w-full rounded-md px-5 py-3 outline-none bg-[var(--gray-500)] text-[var(--gray-200)]"
+                   placeholder="Email"/>
+            <input type="text"
+                   className="w-full rounded-md px-5 py-3 outline-none bg-[var(--gray-500)] text-[var(--gray-200)]"
+                   placeholder="Password"/>
+            <div className="flex justify-between space-x-3 w-full">
+                <button className="w-full bg-[var(--blue)] py-2 rounded-lg">Login</button>
+                <button className="w-full bg-[var(--purple)] py-2 rounded-lg">Register</button>
+            </div>
+            <div className="flex justify-center items-center space-x-5 w-full">
+                <div className="flex-1 h-px bg-[var(--gray-400)]"></div>
+                <img src={GoogleLogo} alt=""/>
+                <div className="flex-1 h-px bg-[var(--gray-400)]"></div>
+            </div>
+
+            <a onClick={props.guestEntry} className="text-[var(--gray-300)] text-sm underline cursor-pointer">Continue as Guest</a>
+        </div>
     )
 }
 
